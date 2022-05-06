b4bfad3000dfd29a4d0e2231deef9e135f4ec980
JHeaon <83858260+JHeaon@users.noreply.github.com>
2022-05-07 05:03:03 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 127564 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,7 +1,14 @@
-n = int(input())
-data = list(map(int, input().split()))
+import sys
 
-for i in range(1, n):
-  data[i] = max(data[i], data[i] + data[i-1])
-  
-print(max(data))
+
+def main():
+    n = int(input())
+    data = list(map(int, sys.stdin.readline().split()))
+    for i in range(1, n):
+        data[i] = max(data[i], data[i] + data[i - 1])
+
+    print(max(data))
+
+
+if __name__ == "__main__":
+    main()
