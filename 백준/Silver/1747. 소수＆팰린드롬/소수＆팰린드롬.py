2d7d92a4599af7393730335ac04876ba91454f69
JHeaon <83858260+JHeaon@users.noreply.github.com>
2022-06-25 21:13:23 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 208 ms, Memory: 115132 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,28 @@
+import sys
+import math
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def prime(num):
+    end = int(math.sqrt(num))
+    for i in range(2, end + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def func():
+    num = int(input())
+    if num == 1:
+        num = 2
+
+    while True:
+        if str(num) == str(num)[::-1] and prime(num):
+            print(num)
+            break
+        else:
+            num += 1
+
+
+if __name__ == "__main__":
+    func()
